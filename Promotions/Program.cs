b94d68e0447e2122b8ee80614dd0eb1291b07783
Nikhil Chaurasia <nikhil.chaurasia2809@gmail.com>
2020-11-03 21:48:28 +0530

    Some changes for checks.

--- Promotions/Program.cs
+++ Promotions/Program.cs
@@ -12,19 +12,30 @@ namespace Promotions
     {
         public static void Main()
         {
-            Console.WriteLine("Please enter the count of distinct SKUs in Cart");
-            int itemsCount;
-            Int32.TryParse(Console.ReadLine(), out itemsCount);
+            
+            int itemsCount=0;
+            do
+            {
+                Console.WriteLine("Please enter the count of distinct SKUs in Cart, should be more than 0");
+                Int32.TryParse(Console.ReadLine(), out itemsCount);
+            }
+            while (itemsCount == 0);
             List<CartItem> items = new List<CartItem>();
             for (int i = 0; i < itemsCount; i++)
             {
                 char SKU;
-                Char.TryParse(Console.ReadLine(), out SKU);
+                string inputString = Console.ReadLine();
+                if(!Char.TryParse(inputString, out SKU))
+                {
+                    SKU = Char.Parse(inputString.Substring(0, 1));
+                }
                 int count;
                 Int32.TryParse(Console.ReadLine(), out count);
                 CartItem item = new CartItem { SKU = SKU, Quantity = count };
                 items.Add(item);
             }
+
+            items.GroupBy(x => x.SKU).Select(x => new CartItem { SKU = x.Key, Quantity = x.Sum(y => y.Quantity) }).ToList();
             CalculatePrice calculatePrice = new CalculatePrice();
             Console.WriteLine(calculatePrice.CalculateTotalPrice(items));
             Console.ReadKey();
